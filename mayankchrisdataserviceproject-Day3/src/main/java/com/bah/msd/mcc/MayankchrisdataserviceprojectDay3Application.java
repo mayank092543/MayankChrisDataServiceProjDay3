34dee1e1d95b1890d5927d91685c1632a71f0ef0
user <user@this.com>
2022-08-15 12:18:13 -0400

    I added the customerApi and customer-domain

---
+++ mayankchrisdataserviceproject-Day3/src/main/java/com/bah/msd/mcc/MayankchrisdataserviceprojectDay3Application.java
@@ -0,0 +1,13 @@
+package com.bah.msd.mcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayankchrisdataserviceprojectDay3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayankchrisdataserviceprojectDay3Application.class, args);
+	}
+
+}
---
+++ mayankchrisdataserviceproject-Day3/src/main/java/com/bah/msd/mcc/api/CustomerApi.java
@@ -0,0 +1,59 @@
+package com.bah.msd.mcc.api;
+
+import java.net.URI;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.bah.msd.mcc.domain.Customer;
+import com.bah.msd.mcc.repository.CustomersRepository;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerApi {
+	
+	@Autowired
+	CustomersRepository repo;
+	
+	@GetMapping
+	public Iterable<Customer> getAll() {
+		return repo.findAll();
+	}
+	
+	@GetMapping("/{customerId}")
+	public Optional<Customer> getCustomerById(@PathVariable("customerId") long id){
+		return repo.findById(id);
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> addCustomer(@RequestBody Customer newCustomer, UriComponentsBuilder uri){
+		if(newCustomer.getId() != 0 || newCustomer.getName() == null || newCustomer.getEmail() == null) {
+			return ResponseEntity.badRequest().build();
+		}
+		newCustomer = repo.save(newCustomer);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newCustomer.getId()).toUri();
+		ResponseEntity<?> response = ResponseEntity.created(location).build();
+		return response;
+		}
+	
+	@PutMapping("/{customerId}")
+	public ResponseEntity<?> updateCustomer(@RequestBody Customer newCustomer, @PathVariable("customerId") long customerId){
+		if(newCustomer.getId() != 0 || newCustomer.getName() == null || newCustomer.getEmail() == null) {
+			return ResponseEntity.badRequest().build();
+		}
+		
+		newCustomer = repo.save(newCustomer);
+		return ResponseEntity.ok().build();
+	}
+}
+
